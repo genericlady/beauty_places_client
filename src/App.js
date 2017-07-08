2e6d4245c37d34dea2e1d0d250b707c970830a12
yianna <yiannakokalas@gmail.com>
2017-07-08 13:48:14 -0400

    Add Reviews
    - Add Reviews constsant to App.js so we can map the values into list
      item elements.
    
    - Create a divs for each place that will be returned as part of the
      place search result.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const REVIEWS = [
+  "I love this place!",
+  "Best experience ever!",
+  "YES! Will come back!"
+]
+
 class App extends Component {
   render() {
+    const reviews = REVIEWS.map(review => React.createElement('li', {}, review))
+
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Beauty Reviews</h2>
+          <h2>Search for Beauty Places</h2>
           <form>
             <label style={{color: "white"}} value="">
               Search by your current location: 
@@ -14,6 +22,39 @@ class App extends Component {
               <input type="submit" value="Submit" />
             </label>
           </form>
+        </div>
+
+        <div className="App-body">
+          <h3>Reviews Found</h3>
+          <div className="Places">
+            <div className="place">
+              <div className="name">Sally's Beauty Place</div>
+              <div className="rating">Rating: ****</div>
+              <div className="reviews">
+                <ul>
+                  {reviews}
+                </ul>
+              </div>
+            </div>
+            <div className="place">
+              <div className="name">A place for your Nails!</div>
+              <div className="rating">Rating: *****</div>
+              <div className="reviews">
+                <ul>
+                  {reviews}
+                </ul>
+              </div>
+            </div>
+            <div className="place">
+              <div className="name">A place for your skin!</div>
+              <div className="rating">Rating: *****</div>
+              <div className="reviews">
+                <ul>
+                  {reviews}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
