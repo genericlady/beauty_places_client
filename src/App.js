f64dffc8727d53dea55cf603a8f26dbbb40cfd9e
yianna <yiannakokalas@gmail.com>
2017-07-08 15:24:28 -0400

    Create BeautyPlace Component
    Abstract all html relevent to a place of beauty into it's own
    BeautyPlace component and file. This will reduce the lines of
    code in the App component making it easier to read.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
-import React, { Component } from 'react';
 import './App.css';
+import React, { Component } from 'react';
+import BeautyPlace from './components/BeautyPlace.js'
 import SearchForm from './components/SearchForm.js'
-
-const REVIEWS = [
-  "I love this place!",
-  "Best experience ever!",
-  "YES! Will come back!"
-]
 
 class App extends Component {
   render() {
-    const reviews = REVIEWS.map(review => React.createElement('li', {}, review))
-
     return (
       <div className="App">
         <div className="App-header">
@@ -22,33 +15,9 @@ class App extends Component {
         <div className="App-body">
           <h3>Reviews Found</h3>
           <div className="Places">
-            <div className="place">
-              <div className="name">Sally's Beauty Place</div>
-              <div className="rating">Rating: ****</div>
-              <div className="reviews">
-                <ul>
-                  {reviews}
-                </ul>
-              </div>
-            </div>
-            <div className="place">
-              <div className="name">A place for your Nails!</div>
-              <div className="rating">Rating: *****</div>
-              <div className="reviews">
-                <ul>
-                  {reviews}
-                </ul>
-              </div>
-            </div>
-            <div className="place">
-              <div className="name">A place for your skin!</div>
-              <div className="rating">Rating: *****</div>
-              <div className="reviews">
-                <ul>
-                  {reviews}
-                </ul>
-              </div>
-            </div>
+            <BeautyPlace name="Sally's Beauty Place" rating="*****" />
+            <BeautyPlace name="A place for your Nails!" rating="*****" />
+            <BeautyPlace name="A place for your skin!" rating="*****" />
           </div>
         </div>
       </div>
