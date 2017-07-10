d2ca47affb730be4d7b6425b7c6daf42a4648e24
yianna <yiannakokalas@gmail.com>
2017-07-10 11:58:39 -0400

    Create a ToggleButton Component.
    Create a ToggleButton combponent with a prop of name so it can have
    specific context of what it is on the button. Add to App.js,
    app-header to render some buttons that will filter results.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Component } from 'react';
 import BeautyPlace from './components/BeautyPlace.js'
 import SearchForm from './components/SearchForm.js'
+import ToggleButton from './components/ToggleButton.js'
 
 class App extends Component {
   render() {
@@ -10,6 +11,10 @@ class App extends Component {
         <div className="App-header">
           <h2>Search for Beauty Places</h2>
           <SearchForm />
+          <ToggleButton name="Hair" />
+          <ToggleButton name="Skin" />
+          <ToggleButton name="Nails" />
+          <ToggleButton name="Medical" />
         </div>
 
         <div className="App-body">
