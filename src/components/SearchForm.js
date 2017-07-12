f07522ef67aaafc9060e4bff09ce51e6a7f98700
yianna <yiannakokalas@gmail.com>
2017-07-12 12:51:52 -0400

    Add typeahead for search form
    - Add styles to App.css for search-typeahead.
    
    - Add a div for search-typeahead and hide it when there is
      either no input or after a form submission. This the
      beginning of a typeahead feature to suggest a location
      for the user to choose from.
    
    - Add prop-types to package.json and run the install. Use
      these PropTypes over React.PropTypes.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,53 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export default class SearchForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loc: '',
+      showTypeahead: false
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  handleChange = event => {
+    this.setState({
+      loc: event.target.value,
+      showTypeahead: event.target.value.length > 0 ? true : false,
+    });
+  }
+
+  handleFormSubmit(ev) {
+    ev.preventDefault();
+
+    this.setState({
+      loc: ev.target.value, 
+      showTypeahead: false
+    });
+  }
+
   render() {
     return (
-      <form>
+      <form onSubmit={this.handleFormSubmit}>
         <label style={{color: "white"}} value="">
           Search by your current location: 
-          <input type="text"></input>
+          <input type="text" onChange={this.handleChange}></input>
           <input type="submit" value="Submit" />
+        <div className={this.state.showTypeahead ? "dropdown" : "invisible"}>
+          <div className="search-typeahead">
+          {this.state.loc}
+          </div>
+        </div>
         </label>
       </form>
     );
   }
 }
+
+SearchForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
