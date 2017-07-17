028a4d8c8c4f0ab11b0c82b6d8ec1e39408fa345
yianna <yiannakokalas@gmail.com>
2017-07-17 17:10:51 -0400

    Add functions for fetching places from Google Places Api.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import './App.css';
 import React, { Component } from 'react';
-import BeautyPlace from './components/BeautyPlace.js'
-import SearchForm from './components/SearchForm.js'
-import ToggleButton from './components/ToggleButton.js'
+import SearchForm from './components/SearchForm.js';
+import Filters from './components/Filters.js';
+import BeautyBrowser from './components/BeautyBrowser.js';
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      beautyPlaces: this.fetchBeautyPlaces(),
+      filters: {
+        type: 'all',
+      }
+    };
+
+    this.handleChangeFilterType = this.handleChangeFilterType.bind(this);
+    this.fetchBeautyPlaces = this.fetchBeautyPlaces.bind(this);
+  }
+
+  fetchBeautyPlaces() {
+    let base = 'https://maps.googleapis.com/maps/api/';
+    let path = 'place/textsearch/json';
+    let query = '?query=hair_care+skin_care+nail_care+in+NYC';
+    let key = '&key=AIzaSyANZpynoTmXN7Xh7XM8g4oVVldbiAolqUs';
+    let url = base + path;
+
+    // if (this.state.filters.type !== 'all') {
+    //   console.log(`?query=skin_care+in+NYC`);
+    //   url += `?type=${this.state.filters.type}`;
+    // }
+
+    var myHeaders = new Headers();
+
+    var myInit = { method: 'GET',
+      headers: myHeaders,
+      mode: 'no-cors',
+      cache: 'default' };
+
+    fetch(url + query + key, myInit)
+      .then(res => res.json())
+      .then(beautyPlaces => this.setState({ beautyPlaces }));
+
+    console.log(this.state.beautyPlaces);
+  }
+
+  handleChangeFilterType() {
+    console.log("FILTER FILTER FILTER");
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h2>Search for Beauty Places</h2>
-          <SearchForm />
-          <ToggleButton name="Hair" />
-          <ToggleButton name="Skin" />
-          <ToggleButton name="Nails" />
-          <ToggleButton name="Medical" />
+          <SearchForm onSubmit={this.fetchBeautyPlaces} />
+          <Filters filters={this.state.filters}
+                   onChangeType={this.handleChangeFilterType} />
         </div>
 
         <div className="App-body">
-          <div className="Places">
-            <BeautyPlace name="Sally's Beauty Place" rating="*****" />
-            <BeautyPlace name="A place for your Nails!" rating="*****" />
-            <BeautyPlace name="A place for your skin!" rating="*****" />
-          </div>
+          <BeautyBrowser beautyPlaces={this.state.beautyPlaces} />
         </div>
       </div>
     );
