8f130d0a9d180a440773e6d734867c40701f1ba6
yianna <yiannakokalas@gmail.com>
2017-07-31 14:52:16 -0400

    Add react router dom for routes
    Add router to App.js since this is the best place to
    load component containers depending upon the route.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './styles/App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
 
 import SearchableBeautyPlacesContainer from './containers/SearchableBeautyPlacesContainer';
 
+const Deals = () => <h1>Deals</h1>
+const About = () => <h1>About</h1>
+
 const App = () => {
   return (
     <div className="App">
-      <SearchableBeautyPlacesContainer />
+      <Router>
+        <div>
+          <Route exact path="/" component={SearchableBeautyPlacesContainer} />
+          <Route exact path="/deals" component={Deals} />
+          <Route exact path="/about" component={About} />
+        </div>
+      </Router>
     </div>
   )
 }
